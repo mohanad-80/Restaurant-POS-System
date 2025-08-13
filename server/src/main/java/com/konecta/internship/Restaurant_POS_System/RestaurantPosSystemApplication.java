94830e62cdd73170393ec72fcca111a480d9c204
Mohanad Ahmed <mhndahmd031@gmail.com>
2025-08-13 19:28:09 +0300

    initial server files

---
+++ server/src/main/java/com/konecta/internship/Restaurant_POS_System/RestaurantPosSystemApplication.java
@@ -0,0 +1,13 @@
+package com.konecta.internship.Restaurant_POS_System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantPosSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantPosSystemApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/konecta/internship/Restaurant_POS_System/RestaurantPosSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.konecta.internship.Restaurant_POS_System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantPosSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
