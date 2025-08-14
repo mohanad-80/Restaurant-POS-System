514d4d9f7908bc499d68e07bd62377c7f7b2fe80
Seif Ahmed Salah <109627927+Seif-Ahmed-Salah@users.noreply.github.com>
2025-08-15 01:04:57 +0300

    Dev2-Day1
    1-MenuItems & Categories CRUD.(Done)
    2-Image upload endpoint.(Done)
    3-Independent DB tables: categories, menuitems.(Done)

---
+++ server/src/main/java/com/konecta/internship/Restaurant_POS_System/Category/CategoryEntity.java
@@ -0,0 +1,31 @@
+package com.konecta.internship.Restaurant_POS_System.Category;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table
+public class CategoryEntity 
+{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @NotBlank
+    private String name;
+
+
+}
---
+++ server/src/main/java/com/konecta/internship/Restaurant_POS_System/Category/CategoryService.java
@@ -0,0 +1,44 @@
+package com.konecta.internship.Restaurant_POS_System.Category;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CategoryService 
+{
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    public List<CategoryEntity> getCategories()
+    {
+        return categoryRepository.findAll();
+    }
+
+    public CategoryEntity addCategory(CategoryEntity category)
+    {
+        return categoryRepository.save(category);
+    }
+
+    public CategoryEntity updateCategory(int id,CategoryEntity category)
+    {
+        CategoryEntity categ= categoryRepository.findById(id).orElseThrow();
+        categ.setName(category.getName());
+        return categ;
+    }
+
+    public void deleteCategory(int id)
+    {
+        boolean isExcist=categoryRepository.findById(id).isPresent();
+        if(!isExcist)
+        {
+            throw new IllegalAccessError("OOPS...This category not found");
+        }
+        else
+        {
+            categoryRepository.deleteById(id);
+        }
+    }
+
+}
