1477d9e4dc416741ef771dc43d37ebef2e0369cb
Mohanad Ahmed <mhndahmd031@gmail.com>
2025-08-14 21:46:24 +0300

    feat: create order controller
    create order controller with two endpoints for getting all orders and creating new order.

---
+++ server/src/main/java/com/konecta/internship/Restaurant_POS_System/orders/controllers/OrderController.java
@@ -0,0 +1,33 @@
+package com.konecta.internship.Restaurant_POS_System.orders.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.konecta.internship.Restaurant_POS_System.orders.dto.OrderRequestDTO;
+import com.konecta.internship.Restaurant_POS_System.orders.entity.Order;
+import com.konecta.internship.Restaurant_POS_System.orders.services.OrderService;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/orders")
+public class OrderController {
+  @Autowired
+  private OrderService orderService;
+
+  @GetMapping
+  public List<Order> getAllOrders(){
+    return orderService.getAllOrders();
+  }
+
+  @PostMapping
+  public Order createOrder(@Valid @RequestBody OrderRequestDTO orderRequest) {
+    return orderService.createOrder(orderRequest);
+  }
+}
