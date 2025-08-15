7994c7c0c372649b93d9407d561502a03e76917a
omarse7a <omarsamehcs@gmail.com>
2025-08-15 11:50:48 +0300

    feat: add database configuration and DiningTable entity

---
+++ server/src/main/java/com/konecta/internship/Restaurant_POS_System/table_management/DiningTable.java
@@ -0,0 +1,26 @@
+package com.konecta.internship.Restaurant_POS_System.table_management;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+
+@Entity
+@Table(name = "dining_tables")
+public class DiningTable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "table_number", nullable = false, unique = true)
+    private String tableNumber;
+    @Column(name = "seats", nullable = false)
+    private int seats;
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status")
+    private TableStatus status = TableStatus.AVAILABLE;
+}
