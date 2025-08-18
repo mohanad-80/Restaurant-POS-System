c6d1878cd874ccfbab97d9c8449a2448ed4e0244
Omar Elshereef <omarelshereef@gmail.com>
2025-08-18 17:25:29 +0300

    setup logout functionallity
    not finished

--- server/src/main/java/com/konecta/internship/Restaurant_POS_System/auth/security/jwt/exception/JwtTokenInvalidException.java
+++ server/src/main/java/com/konecta/internship/Restaurant_POS_System/auth/security/jwt/exception/JwtTokenInvalidException.java
@@ -1,6 +1,9 @@
 package com.konecta.internship.Restaurant_POS_System.auth.security.jwt.exception;
 
 public class JwtTokenInvalidException extends RuntimeException {
+    public JwtTokenInvalidException(String message) {
+        super(message);
+    }
     public JwtTokenInvalidException(String message, Throwable cause) {
         super(message, cause);
     }
