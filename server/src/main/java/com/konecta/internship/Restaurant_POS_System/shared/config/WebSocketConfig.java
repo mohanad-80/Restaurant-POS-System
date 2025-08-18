dea040f615e3b96937fc50cfa0c80732c693ae1e
omarse7a <omarsamehcs@gmail.com>
2025-08-18 22:29:01 +0300

    fix: change STOMP allowed origins

--- server/src/main/java/com/konecta/internship/Restaurant_POS_System/shared/config/WebSocketConfig.java
+++ server/src/main/java/com/konecta/internship/Restaurant_POS_System/shared/config/WebSocketConfig.java
@@ -19,7 +19,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
   @Override
   public void registerStompEndpoints(StompEndpointRegistry registry) {
     registry.addEndpoint("/ws") // the URL for frontend to connect
-            .setAllowedOrigins("*")
+            .setAllowedOrigins("http://127.0.0.1:3001", "http://127.0.0.1:3000")
             .withSockJS();
   }
 }
